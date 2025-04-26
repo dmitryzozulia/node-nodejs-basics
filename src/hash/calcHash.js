d614fd3d61df8cc8b5c529f308fce49167980e03
Dmytro Zozulia <z1dima1z@gmail.com>
2025-04-26 19:56:30 +0200

    feat: add solution to hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const stream = createReadStream("./files/fileToCalculateHashFor.txt");
+
+  stream.on("readable", () => {
+    const data = stream.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log("File hash: ", hash.digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
+
