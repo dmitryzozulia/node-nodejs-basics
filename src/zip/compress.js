e14db7323569c5746c6cd4d5bee6fe89a59184bd
Dmytro Zozulia <z1dima1z@gmail.com>
2025-04-27 15:46:57 +0200

    feat: add solution to zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  if (fs.existsSync("./files/fileToCompress.txt")) {
+    const resource = fs.createReadStream("./files/fileToCompress.txt");
+    const destination = fs.createWriteStream("./files/archive.gz");
+    const compress = zlib.createGzip();
+    resource.pipe(compress).pipe(destination);
+    destination.on("finish", async () => {
+      await fs.promises.unlink("./files/fileToCompress.txt");
+    });
+    console.log("File was succesfully compressed. Source file was deleted!");
+  } else {
+    console.error("No source file!");
+  }
 };
 
 await compress();
+
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import zlib from "zlib";
+import { pipeline } from "stream/promises";
+
 const decompress = async () => {
-    // Write your code here 
+  if (fs.existsSync("./files/archive.gz")) {
+    const source = fs.createReadStream("./files/archive.gz");
+    const destination = fs.createWriteStream("./files/fileToCompress.txt");
+    const decompressStream = zlib.createGunzip();
+    await pipeline(source, decompressStream, destination);
+    console.log("File successfully decompressed. Source file was deleted");
+    await fs.promises.unlink("./files/archive.gz");
+  } else {
+    console.error("No files to decompress. Please add archive.gz");
+  }
 };
 
 await decompress();
+
