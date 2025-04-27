a2740ea17c6c1a65885917f6f1b9e30f63b0cf92
Dmytro Zozulia <z1dima1z@gmail.com>
2025-04-27 17:38:06 +0200

    feat: add worker solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = Array.from({ length: cpus().length }, (_, index) => {
+    return new Promise((resolve) => {
+      const worker = new Worker("./worker.js");
+      worker.postMessage(10 + index);
+
+      worker.on("message", (message) => {
+        resolve(message);
+      });
+
+      worker.on("error", () => {
+        resolve({ status: "error", data: null });
+      });
+    });
+  });
+
+  const results = await Promise.all(workers);
+  console.log(results);
 };
 
 await performCalculations();
+
